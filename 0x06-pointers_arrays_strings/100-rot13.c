231b44753644b1f007add08174af2bdb515c2a81
NohaHamdy <nohatota99@gmail.com>
2023-03-02 09:04:17 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  *rot13 - a function that encodes a string using rot13
  *@n: a string to be encoded
@@ -7,22 +6,18 @@
  */
 char *rot13(char *n)
 {
-	int i;
+	int i = 0;
 
 	while (n[i] != '\0')
 	{
-		printf("Inside first while loop\n");
 		while ((n[i] >= 'a' && n[i] <= 'z') || (n[i] >= 'A' && n[i] <= 'Z'))
 		{
-			printf("Inside second while loop\n");
 			if ((n[i] >= 'a' && n[i] <= 'm') || (n[i] >= 'A' && n[i] <= 'M'))
 			{
 				n[i] += 13;
-				printf("Inside if\n");
 			} else
 			{
 				n[i] -= 13;
-				printf("Inside else\n");
 			}
 			i++;
 		}
