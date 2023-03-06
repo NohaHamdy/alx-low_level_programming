40d03b542f19959e66c0a98909a6510d24708ecc
NohaHamdy <nohatota99@gmail.com>
2023-03-06 07:02:12 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+/*#include <stddef.h>*/
 /**
  *_strchr - a function that locates a character in a string
  *@s: a string
@@ -19,7 +19,7 @@ char *_strchr(char *s, char c)
 		}
 	}
 	if (s[i] == '\0')
-		return (NULL);
+		return (0);
 	else
 		return (&s[i]);
 }
