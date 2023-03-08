0ab0c65673561313d6e5f6063602627a808a18db
NohaHamdy <nohatota99@gmail.com>
2023-03-08 11:02:53 -0800

    Dreams feel real while we're in them.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen_recursion - a function that returns the length of a string
+ *@s: a string
+ *Return: returns the length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		++i;
+		i += _strlen_recursion(s + 1);
+		return (i);
+	}
+	return (i);
+}
