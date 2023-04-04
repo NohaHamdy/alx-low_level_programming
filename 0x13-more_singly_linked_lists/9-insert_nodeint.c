1a0157d84b3ba6962fad8e50a26bb5d39fe20271
NohaHamdy <nohatota99@gmail.com>
2023-04-04 15:13:55 -0700

    Insert 6th try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,9 +15,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int counter = 0;
 	listint_t *new_node;
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
