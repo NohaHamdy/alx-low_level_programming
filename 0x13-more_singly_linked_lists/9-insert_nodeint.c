93eb36ab441d3b7613d8ee5b7c2a836909a53b75
NohaHamdy <nohatota99@gmail.com>
2023-04-04 15:09:38 -0700

    Insert5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int counter = 0;
 	listint_t *new_node;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
