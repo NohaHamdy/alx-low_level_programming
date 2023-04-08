43a85d7575adb255116b495001810395595576e6
NohaHamdy <nohatota99@gmail.com>
2023-04-08 08:42:48 -0700

    0_back to second try

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 	L = strlen(b);
 	for (i = L - 1 ; i >= 0; i--)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
