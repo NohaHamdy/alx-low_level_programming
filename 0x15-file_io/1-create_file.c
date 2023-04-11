931545d17eb3c8c796d1074315e91d5ff23e1f8d
NohaHamdy <nohatota99@gmail.com>
2023-04-10 17:57:01 -0700

    Speak gently, she can hear_1st try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	
 	fd = open(filename, O_RDONLY);
 	if (fd != -1)
 	{
@@ -24,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_CREAT | O_RDWR, 0600);
 	}
+	/*fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);*/
 	L = strlen(text_content);
 	WR = write(fd, text_content, L);
 
